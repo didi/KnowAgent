937c80bf2a208202de00f1a55624bce62527fc54
zhangyu <yzzhangyu_v@didiglobal.com>
2021-07-06 20:05:55 +0800

    修改error log的字段长度

---
+++ agent-manager-v2/agent-manager-common/src/main/java/com/didichuxing/datachannel/agentmanager/common/bean/common/AgentMetricField.java
@@ -0,0 +1,70 @@
+package com.didichuxing.datachannel.agentmanager.common.bean.common;
+
+public class AgentMetricField {
+    // agent维度
+    public static final String CPU_USAGE = "cpu_usage";
+
+    public static final String HEARTBEAT_TIME = "heartbeat_time";
+
+    public static final String HOST_IP = "host_ip";
+
+    public static final String CPU_LIMIT = "cpu_limit";
+
+    public static final String GC_COUNT = "gc_count";
+
+    public static final String PATH_ID = "path_id";
+
+    public static final String LOG_MODE_ID = "log_mode_id";
+
+    public static final String HOSTNAME = "hostname";
+
+    public static final String FD_COUNT = "fd_count";
+
+    public static final String LIMIT_TPS = "limit_tps";
+
+    public static final String MEMORY_USAGE = "memory_usage";
+
+    public static final String START_TIME = "start_time";
+
+    public static final String LOG_PATH_KEY = "log_path_key";
+
+    public static final String MESSAGE_VERSION = "message_version";
+
+    // 采集任务维度
+    public static final String READ_TIME_MEAN = "read_time_mean";
+
+    public static final String FILTER_REMAINED = "filter_remained";
+
+    public static final String CHANNEL_CAPACITY = "channel_capacity";
+
+    public static final String IS_FILE_EXIST = "is_file_exist";
+
+    public static final String TYPE = "type";
+
+    public static final String READ_COUNT = "read_count";
+
+    public static final String SEND_TIME_MEAN = "send_time_mean";
+
+    public static final String MASTER_FILE = "master_file";
+
+    public static final String PATH = "path";
+
+    public static final String SINK_NUM = "sink_num";
+
+    public static final String FLUSH_TIME_MEAN = "flush_time_mean";
+
+    public static final String LATEST_FILE = "latest_file";
+
+    public static final String FILTER_TOO_LARGE_COUNT = "filter_too_large_count";
+
+    public static final String CHANNEL_TYPE = "channel_type";
+
+    public static final String LOG_MODEL_VERSION = "log_model_version";
+
+    public static final String TOPIC = "topic";
+
+    public static final String FLUSH_COUNT = "flush_count";
+
+
+
+}
