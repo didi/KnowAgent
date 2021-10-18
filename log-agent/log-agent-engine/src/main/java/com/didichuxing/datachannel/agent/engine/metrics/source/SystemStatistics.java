f700b3f3711dba99936b57d0adbab8531b54cd20
huqidong <williamhuqidong@didiglobal.com>
2021-10-18 11:58:36 +0800

    Log-agent 添加SystemStatistics类.

---
+++ log-agent/log-agent-engine/src/main/java/com/didichuxing/datachannel/agent/engine/metrics/source/SystemStatistics.java
@@ -0,0 +1,21 @@
+package com.didichuxing.datachannel.agent.engine.metrics.source;
+
+import com.didichuxing.datachannel.agent.common.metrics.MetricsBuilder;
+
+public class SystemStatistics extends AbstractStatistics {
+
+    public SystemStatistics(String name) {
+        super(name);
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void getMetrics(MetricsBuilder builder, boolean all) {
+
+    }
+
+}
