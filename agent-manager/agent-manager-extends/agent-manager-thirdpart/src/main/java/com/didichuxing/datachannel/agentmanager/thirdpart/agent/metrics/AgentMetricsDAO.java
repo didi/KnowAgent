9687bdaf7e79a24f533ef8e724fa7eaef8710d91
huqidong <williamhuqidong@didiglobal.com>
2022-04-05 19:11:01 +0800

    1.）补充进程级指标

--- agent-manager/agent-manager-extends/agent-manager-thirdpart/src/main/java/com/didichuxing/datachannel/agentmanager/thirdpart/agent/metrics/AgentMetricsDAO.java
+++ agent-manager/agent-manager-extends/agent-manager-thirdpart/src/main/java/com/didichuxing/datachannel/agentmanager/thirdpart/agent/metrics/AgentMetricsDAO.java
@@ -2,7 +2,6 @@ package com.didichuxing.datachannel.agentmanager.thirdpart.agent.metrics;
 
 import com.didichuxing.datachannel.agentmanager.common.bean.domain.agent.metrics.DashBoardStatisticsDO;
 import com.didichuxing.datachannel.agentmanager.common.bean.po.metrics.MetricsLogCollectTaskPO;
-import com.didichuxing.datachannel.agentmanager.common.enumeration.metrics.AgentMetricField;
 import com.didichuxing.datachannel.agentmanager.common.enumeration.metrics.AggregationCalcFunctionEnum;
 import com.didichuxing.datachannel.agentmanager.common.bean.vo.metrics.MetricPoint;
 import com.didichuxing.datachannel.agentmanager.common.exception.ServiceException;
@@ -236,22 +235,6 @@ public interface AgentMetricsDAO {
     List<MetricPoint> getCollectDelayPerMin(Long logCollectTaskId, Long fileLogCollectPathId, String logModelHostName, Long startTime, Long endTime);
 
     List<MetricPoint> getAgentErrorLogCountPerMin(String hostName, Long startTime, Long endTime);
-
-    List<MetricPoint> queryByTask(Long logCollectTaskId, Long startTime, Long endTime, AgentMetricField column);
-
-    List<MetricPoint> queryAggregationByTask(Long logCollectTaskId, Long startTime, Long endTime, AgentMetricField column, AggregationCalcFunctionEnum method, int step);
-
-    List<MetricPoint> queryAggregationByHostname(String hostname, Long startTime, Long endTime, AgentMetricField column, AggregationCalcFunctionEnum method, int step);
-
-    List<MetricPoint> queryByLogModel(Long logCollectTaskId, Long fileLogCollectPathId, String logModelHostName, Long startTime, Long endTime, AgentMetricField column);
-
-    List<MetricPoint> queryAggregationByLogModel(Long logCollectTaskId, Long fileLogCollectPathId, String logModelHostName, Long startTime, Long endTime, AgentMetricField column, AggregationCalcFunctionEnum method, int step);
-
-    List<MetricPoint> queryAgent(String hostname, Long startTime, Long endTime, AgentMetricField column);
-
-    List<MetricPoint> queryAgentAggregation(String hostname, Long startTime, Long endTime, AgentMetricField column, AggregationCalcFunctionEnum method, int step);
-
-    Double queryAggregationForAll(Long startTime, Long endTime, AgentMetricField column, AggregationCalcFunctionEnum method);
 
     MetricsLogCollectTaskPO selectLatestMetric(Long taskId);
 
--- agent-manager/agent-manager-extends/agent-manager-thirdpart/src/main/java/com/didichuxing/datachannel/agentmanager/thirdpart/agent/metrics/impl/AgentMetricsRDSImpl.java
+++ agent-manager/agent-manager-extends/agent-manager-thirdpart/src/main/java/com/didichuxing/datachannel/agentmanager/thirdpart/agent/metrics/impl/AgentMetricsRDSImpl.java
@@ -5,7 +5,6 @@ import com.alibaba.fastjson.JSONObject;
 import com.didichuxing.datachannel.agentmanager.common.bean.domain.agent.metrics.DashBoardStatisticsDO;
 import com.didichuxing.datachannel.agentmanager.common.bean.po.agent.ErrorLogPO;
 import com.didichuxing.datachannel.agentmanager.common.bean.po.metrics.*;
-import com.didichuxing.datachannel.agentmanager.common.enumeration.metrics.AgentMetricField;
 import com.didichuxing.datachannel.agentmanager.common.enumeration.metrics.AggregationCalcFunctionEnum;
 import com.didichuxing.datachannel.agentmanager.common.bean.vo.metrics.MetricPoint;
 import com.didichuxing.datachannel.agentmanager.common.exception.ServiceException;
@@ -263,54 +262,6 @@ public class AgentMetricsRDSImpl implements AgentMetricsDAO {
     @Override
     public List<MetricPoint> getAgentErrorLogCountPerMin(String hostname, Long startTime, Long endTime) {
 //        return agentMetricMapper.selectAgentErrorLogCountPerMin(hostname, startTime, endTime);
-        return null;
-    }
-
-    @Override
-    public List<MetricPoint> queryByTask(Long logCollectTaskId, Long startTime, Long endTime, AgentMetricField column) {
-//        return collectTaskMetricMapper.selectByTask(logCollectTaskId, startTime, endTime, column.getRdsValue());
-        return null;
-    }
-
-    @Override
-    public List<MetricPoint> queryAggregationByTask(Long logCollectTaskId, Long startTime, Long endTime, AgentMetricField column, AggregationCalcFunctionEnum method, int step) {
-//        return collectTaskMetricMapper.selectAggregationByTask(logCollectTaskId, startTime, endTime, column.getRdsValue(), method.getValue(), step);
-        return null;
-    }
-
-    @Override
-    public List<MetricPoint> queryAggregationByHostname(String hostname, Long startTime, Long endTime, AgentMetricField column, AggregationCalcFunctionEnum method, int step) {
-//        return collectTaskMetricMapper.selectAggregationByHostname(hostname, startTime, endTime, column.getRdsValue(), method.getValue(), step);
-        return null;
-    }
-
-    @Override
-    public List<MetricPoint> queryByLogModel(Long logCollectTaskId, Long fileLogCollectPathId, String logModelHostName, Long startTime, Long endTime, AgentMetricField column) {
-//        return collectTaskMetricMapper.selectByLogModel(logCollectTaskId, logModelHostName, fileLogCollectPathId, startTime, endTime, column.getRdsValue());
-        return null;
-    }
-
-    @Override
-    public List<MetricPoint> queryAggregationByLogModel(Long logCollectTaskId, Long fileLogCollectPathId, String logModelHostName, Long startTime, Long endTime, AgentMetricField column, AggregationCalcFunctionEnum method, int step) {
-//        return collectTaskMetricMapper.selectAggregationByLogModel(logCollectTaskId, logModelHostName, fileLogCollectPathId, startTime, endTime, column.getRdsValue(), method.getValue(), step);
-        return null;
-    }
-
-    @Override
-    public List<MetricPoint> queryAgent(String hostname, Long startTime, Long endTime, AgentMetricField column) {
-//        return agentMetricMapper.selectGraph(hostname, startTime, endTime, column.getRdsValue());
-        return null;
-    }
-
-    @Override
-    public List<MetricPoint> queryAgentAggregation(String hostname, Long startTime, Long endTime, AgentMetricField column, AggregationCalcFunctionEnum method, int step) {
-//        return agentMetricMapper.selectAggregation(hostname, startTime, endTime, column.getRdsValue(), method.getValue(), step);
-        return null;
-    }
-
-    @Override
-    public Double queryAggregationForAll(Long startTime, Long endTime, AgentMetricField column, AggregationCalcFunctionEnum method) {
-//        return collectTaskMetricMapper.selectAggregationForAll(startTime, endTime, column.getRdsValue(), method.getValue());
         return null;
     }
 
